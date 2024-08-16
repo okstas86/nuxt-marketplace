8424ed8cd2b79363fdb5511d25f0fc1b15f0c370
okstas86 <stanislavocunev1@gmail.com>
2024-08-16 15:22:55 +0300

    update readme file

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Nuxt 3 Minimal Starter
+# Nuxt Marketplace
 
-Look at the [Nuxt 3 documentation](https://nuxt.com/docs/getting-started/introduction) to learn more.
+<img align="left" alt="screen" width="1100" src="./public/image/home.png" />
 
 ## Setup
 
--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="nav-bar" class="flex w-full items-center justify-between border-b border-gray-300 h-20 px-8">
-    <NuxtLink to="/" id="home-link" class="text-black text-xl uppercase no-underline hover:text-[#ff8e01]">Fabulous Footwear</NuxtLink>
+    <NuxtLink to="/" id="home-link" class="text-black text-xl uppercase no-underline hover:text-[#ff8e01] border-none">Fabulous Footwear</NuxtLink>
     <div class="flex gap-8 items-center">
       <NuxtLink to="/products" id="products-link" class="text-black text-base hover:text-[#ff8e01] uppercase no-underline">Products</NuxtLink>
       <NuxtLink to="/cart" id="cart-link" class="relative text-black text-base uppercase no-underline hover:text-[#ff8e01]">
